e939f98f668c3966f0bb9c72ac6ba35a403a2a90
rnnc <rohan_cy@outlook.com>
2019-02-09 22:46:55 -0500

    added trends api interacting functions

---
+++ trendsData.js
@@ -0,0 +1,20 @@
+const googleTrends = require('google-trends-api');
+
+module.exports.getTrendData = async (term1, term2) => {
+
+  const startDate = new Date();
+  startDate.setMonth(startDate.getMonth() - 12);
+
+  let result;
+  try {
+    result = await googleTrends.interestOverTime({
+      keyword: [term1, term2],
+      startTime: startDate
+    })
+  } catch (error) {
+    throw `Error receiving trends data\n${error}`;
+  }
+
+  return JSON.parse(result);
+
+}
